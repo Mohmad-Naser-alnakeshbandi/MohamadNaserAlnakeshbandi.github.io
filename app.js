9043795d357c42cbaaf5ce0a1ef7843824658590
Mohmad Naser Alnakeshbandi <76824323+Mohmad-Naser-alnakeshbandi@users.noreply.github.com>
2022-01-09 18:06:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+function facebook() {
+  window.open("https://www.facebook.com/profile.php?id=100010782363745");
+}
+function Insta() {
+  window.open("https://www.instagram.com/mohmad_alnakeshbandi/");
+}
+function linkedin() {
+  window.open(
+    "https://www.linkedin.com/in/mohamad-naser-alnakeshbandi-0b61a6222/"
+  );
+}
+function git() {
+  window.open("https://github.com/Mohmad-Naser-alnakeshbandi");
+}
